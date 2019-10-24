c250c1498b511003163fdb98a75b3e0ae9c207a0
mbobovnikov <mbobovnikov@gmail.com>
2019-10-24 07:05:22 +0300

    Пробный шаблон, потом будет заменено на действующий проект

--- CourseWork2/ViewModel/ViewModelRes.cs
+++ CourseWork2/ViewModel/ViewModelRes.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using CourseWork2.Model;
 
 namespace CourseWork2.ViewModel
 {
-    class ViewModelRes
+   public class ViewModelRes: INotifyPropertyChanged
     {
+        private string helloString;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return helloString;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Raises OnPropertychangedEvent when property changes
+        /// </summary>
+        /// <param name="name">String representing the property name</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ViewModelRes()
+        {
+            CourseResitens helloWorldModel = new CourseResitens();
+            helloString = helloWorldModel.ImportantInfo;
+        }
     }
 }
